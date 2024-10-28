36bc5653cbe91f3909b4508d33b209b9986aa183
𝐊𝐈𝐍𝐆 𝐒𝐇𝐎𝐔𝐑𝐎𝐕 𝐁𝐎𝐓 <164044359+sormishourovss@users.noreply.github.com>
2024-10-28 19:07:52 +0300

    Update egboss.js

--- scripts/commands/egboss.js
+++ scripts/commands/egboss.js
@@ -16,9 +16,9 @@ handleEvent: function({ api, event, client, __GLOBAL }) {
   var { threadID, messageID } = event;
   const content = event.body ? event.body : '';
   const body = content.toLowerCase();
-  if (body.indexOf("Farhan")==0 || body.indexOf("FARHAN")==0 || body.indexOf("ফারহান")==0 || body.indexOf("farhan")==0) {
+  if (body.indexOf("Farhan")==0 || body.indexOf("SHOUROV")==0 || body.indexOf("সৌরভ")==0 || body.indexOf("shourov")==0) {
     var msg = {
-        body: "ফা্ঁর্ঁহা্ঁন্ঁ এ্ঁখ্ঁন্ঁ খু্ঁব্ঁ বি্ঁজি্ঁ 🥰❤️🥀",
+        body: " এ্ঁখ্ঁন্ঁ সৌরভ খু্ঁব্ঁ বি্ঁজি্ঁ 🥰❤️🥀",
         attachment: fs.createReadStream(__dirname + `/Nayan/boss.mp3`)
       }
       api.sendMessage( msg, threadID, messageID);
